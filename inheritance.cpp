480c32f291725881885a5fd2482a9ce7c1d0a57e
anderson <anderson.t.peng@gmail.com>
2017-09-22 00:35:28 -0500

    first commit

---
+++ inheritance.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+using namespace std;
+
+class Mother {
+    public:
+        Mother()
+        {
+            cout <<"Mother ctor"<<endl;
+        }
+        ~Mother()
+        {
+            cout <<"Mother dtor"<<endl;
+        }
+};
+
+class Daughter: public Mother {
+    public:
+        Daughter()
+        {
+            cout <<"Daughter ctor"<<endl;
+        }
+        ~Daughter()
+        {
+            cout <<"Daughter dtor"<<endl;
+        }
+};
+
+int main() {
+    Daughter m;
+}
+
+//Mother ctor
+//Daughter ctor
+//Daughter dtor
+//Mother dtor
